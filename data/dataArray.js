31a06411588305e5cf6fd101e1e170a51f255712
Eugene-Damiani <damianieugene@gmail.com>
2021-03-02 14:23:49 -0800

    added vendors to the array

--- data/dataArray.js
+++ data/dataArray.js
@@ -1,9 +1,9 @@
   export const buisnesses = [
     {
-      buisnessId: 122,
+      buisnessId: 1,
       categoryId: 3,
-      title: 'Costco',
-      photo_url: 'https://media-exp1.licdn.com/dms/image/C4E1BAQHQKqO0DbY4OA/company-background_10000/0/1553094224126?e=2159024400&v=beta&t=evGEs9tBiyh_LUTfJJee_W-KEldUj9LoyFEFf2aq8Ec',
+      title: 'South Central Farmers',
+      photo_url: 'https://www.southcentralfarmers.com/scfcoop/shop/product_images/Capture.PNG',
       photosArray: [
         'https://media-exp1.licdn.com/dms/image/C4E1BAQHQKqO0DbY4OA/company-background_10000/0/1553094224126?e=2159024400&v=beta&t=evGEs9tBiyh_LUTfJJee_W-KEldUj9LoyFEFf2aq8Ec',
         "https://namelymarly.com/wp-content/uploads/2018/04/20180415_Beet_Lasagna_10.jpg",
@@ -11,29 +11,40 @@
         'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492718105/articles/2013/09/24/burger-king-s-new-french-fries-took-ten-years-to-develop/130923-gross-burger-tease_izz59e',
         'https://aht.seriouseats.com/images/2012/02/20120221-193971-fast-food-fries-Burger-King-fries-2.jpg'
       ],
-      time: '15',
-      ingredients: [[0, '200ml'], [1, '5g'], [2, '300g']],
-      description:
-       'MAP goes here'
+      contact: 'Phone: (818)-861-9470' + 'Based in Los Angeles 90058',
+      siteUrl: 'https://www.scfcoop.southcentralfarmers.com/',
+      description: 'No minimum. Email / Delivery. Sunday deliveries only. Orders must be made on Friday prior to delivery. Invoice is sent via PayPal and must be paid on receipt. No Minimum order requirement.' + 'MAP goes here'
     },
     {
-      buisnessId: 3,
+      buisnessId: 2,
       categoryId: 4,
-      title: 'Jacks Corner Market',
+      title: 'Valley Produce',
       photo_url:
-        'https://www.gannett-cdn.com/presto/2020/01/24/PDTF/eabf83a5-85f5-4edb-bea8-b1f41bb555ff-012420_woodward_corner_market_rg_01.JPG?crop=5934,3338,x0,y619&width=3200&height=1801&format=pjpg&auto=webp',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS5EZrmslGZfXn2JA3GocimlAYAyaRXE8TpQ&usqp=CAU',
       photosArray: [
         'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
         'https://www.vitamix.com/media/other/images/xVitamix-Triple-Berry-Smoothie-square-crop__1.jpg.pagespeed.ic.OgTC3ILD3R.jpg',
         'http://images.media-allrecipes.com/userphotos/960x960/3798204.jpg',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzui8MM6W66I29VZwVvcjpGv99JW3O1owgupc3KwB65rhAyrZ'
       ],
-      time: '10',
-      ingredients: [
-        [59, '1'],
-        [60, '1/2 lbs'],
-        [61, '1/2 liters'],
+      contact: 'Aaron Williams, Phone (213)627-8736, Hours: 7AM to 1PM, Email: apw@valleyproduce.com Address: Los Angeles 90021' ,
+      siteUrl: 'https://www.valleyproduce.com/',
+      description: 'Contact Aaron for questions and set up an account. Ordering is done online. No minimum order requirement. Offers delivery. Schedule to be determined. Carries a wide range of fruits and vegetables as well as ready-to-eat items such pre-cut fruit and salads.'
+    },
+    {
+      buisnessId: 3,
+      categoryId: 5,
+      title: 'World Wide Produce',
+      photo_url:
+        'https://www.wwproduce.com/main/wp-content/uploads/2019/08/WWP_LOGO-blue-text-only.png',
+      photosArray: [
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
+        'https://www.vitamix.com/media/other/images/xVitamix-Triple-Berry-Smoothie-square-crop__1.jpg.pagespeed.ic.OgTC3ILD3R.jpg',
+        'http://images.media-allrecipes.com/userphotos/960x960/3798204.jpg',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzui8MM6W66I29VZwVvcjpGv99JW3O1owgupc3KwB65rhAyrZ'
       ],
-      description: 'In a blender, combine all ingredients and blend until smooth. Then divide between 2 cups and top with blackberries, if desired.'
+      contact: 'Reyes Phone: (213)290-8866 Address: Los Angeles 90056',
+      siteUrl: 'https://www.wwproduce.com/',
+      description: 'Online Delivery Deliveries available 4 Days Week except Wednesday & Sundays. Minimum delivery: $250 Daily or $1,500 Weekly.'
     },
   ];
