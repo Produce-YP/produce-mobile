0679904599cb392f8d02cacea794ee7f79d1c8c1
Eugene-Damiani <damianieugene@gmail.com>
2021-03-18 11:12:26 -0700

    added pictures

--- data/dataArray.js
+++ data/dataArray.js
@@ -2,21 +2,16 @@
     {
       buisnessId: 1,
       categoryId: 3,
+      priceRating: '$$$', 
       title: 'South Central Farmers',
       photo_url: 'https://www.southcentralfarmers.com/scfcoop/shop/product_images/Capture.PNG',
       photosArray: [
-        'https://media-exp1.licdn.com/dms/image/C4E1BAQHQKqO0DbY4OA/company-background_10000/0/1553094224126?e=2159024400&v=beta&t=evGEs9tBiyh_LUTfJJee_W-KEldUj9LoyFEFf2aq8Ec',
-        "https://namelymarly.com/wp-content/uploads/2018/04/20180415_Beet_Lasagna_10.jpg",
-        'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width600/img/news_impact/photo/burger-fijpg-57e7e5907630c2ad.jpg',
-        'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492718105/articles/2013/09/24/burger-king-s-new-french-fries-took-ten-years-to-develop/130923-gross-burger-tease_izz59e',
-        'https://aht.seriouseats.com/images/2012/02/20120221-193971-fast-food-fries-Burger-King-fries-2.jpg'
+        'https://www.southcentralfarmers.com/scfcoop/shop/product_images/Capture.PNG',
+        'https://www.southcentralfarmers.com/scfcoop/shop/product_images/uploaded_images/capture.png'
       ],
-      contact: {
-        phone: '(818)-861-9470',
-        address: 'Based in Los Angeles 90058'
-      },
+      contact: 'Phone: (818)-861-9470' + 'Based in Los Angeles 90058',
       siteUrl: 'https://www.scfcoop.southcentralfarmers.com/',
-      description: 'No minimum. Email / Delivery. Sunday deliveries only. Orders must be made on Friday prior to delivery. Invoice is sent via PayPal and must be paid on receipt. No Minimum order requirement.'
+      description: 'No minimum. Email / Delivery. Sunday deliveries only. Orders must be made on Friday prior to delivery. Invoice is sent via PayPal and must be paid on receipt. No Minimum order requirement.' + 'MAP goes here'
     },
     {
       buisnessId: 2,
@@ -25,18 +20,15 @@
       photo_url:
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS5EZrmslGZfXn2JA3GocimlAYAyaRXE8TpQ&usqp=CAU',
       photosArray: [
-        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
-        'https://www.vitamix.com/media/other/images/xVitamix-Triple-Berry-Smoothie-square-crop__1.jpg.pagespeed.ic.OgTC3ILD3R.jpg',
-        'http://images.media-allrecipes.com/userphotos/960x960/3798204.jpg',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzui8MM6W66I29VZwVvcjpGv99JW3O1owgupc3KwB65rhAyrZ'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS5EZrmslGZfXn2JA3GocimlAYAyaRXE8TpQ&usqp=CAU',
+        'https://www.valleyproduce.com/wp-content/uploads/2019/09/asian.jpg',
+        'https://www.valleyproduce.com/wp-content/uploads/2019/09/latin.jpg',
+        'https://www.valleyproduce.com/wp-content/uploads/2016/10/Certified-Organic-Handler-Logo.jpg',
+        'https://www.valleyproduce.com/wp-content/uploads/2017/08/Apricots.png',
+        'https://www.valleyproduce.com/wp-content/uploads/2016/12/Honeycrisp.png',
+        'https://www.valleyproduce.com/wp-content/uploads/2016/07/Dragon-Fruit.jpg'
       ],
-      contact: {
-        name: 'Aaron Williams',
-        phone: '(213)627-8736',
-        hours: '7AM to 1PM',
-        email: 'apw@valleyproduce.com',
-        address: 'Los Angeles 90021'
-      },
+      contact: 'Aaron Williams, Phone (213)627-8736, Hours: 7AM to 1PM, Email: apw@valleyproduce.com Address: Los Angeles 90021' ,
       siteUrl: 'https://www.valleyproduce.com/',
       description: 'Contact Aaron for questions and set up an account. Ordering is done online. No minimum order requirement. Offers delivery. Schedule to be determined. Carries a wide range of fruits and vegetables as well as ready-to-eat items such pre-cut fruit and salads.'
     },
@@ -47,18 +39,11 @@
       photo_url:
         'https://www.wwproduce.com/main/wp-content/uploads/2019/08/WWP_LOGO-blue-text-only.png',
       photosArray: [
-        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
-        'https://www.vitamix.com/media/other/images/xVitamix-Triple-Berry-Smoothie-square-crop__1.jpg.pagespeed.ic.OgTC3ILD3R.jpg',
-        'http://images.media-allrecipes.com/userphotos/960x960/3798204.jpg',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzui8MM6W66I29VZwVvcjpGv99JW3O1owgupc3KwB65rhAyrZ'
+        'https://www.wwproduce.com/main/wp-content/uploads/2019/08/WWP_LOGO-blue-text-only.png',
+        'https://www.wwproduce.com/main/wp-content/uploads/2015/09/OrganicsSS-Post1.png',
+        'https://www.wwproduce.com/main/wp-content/uploads/2014/09/Veggies2x1-1024x538.jpg'
       ],
-      contact: {
-        name: 'Reyes ',
-        phone: '(213)290-8866',
-        hours: '',
-        email: '',
-        address: 'Los Angeles 90056'
-      },
+      contact: 'Reyes Phone: (213)290-8866 Address: Los Angeles 90056',
       siteUrl: 'https://www.wwproduce.com/',
       description: 'Online Delivery Deliveries available 4 Days Week except Wednesday & Sundays. Minimum delivery: $250 Daily or $1,500 Weekly.'
     },
